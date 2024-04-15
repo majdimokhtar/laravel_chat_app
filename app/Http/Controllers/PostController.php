0318a0d684819bc0b4226deff4adfe7f35de84d8
Majdi Mokhtar <majdi.mokhtar@gmail.com>
2024-04-15 15:21:58 +0100

    implementing markdown

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     //
     public function viewSinglePost(Post $post) {
+        $post['body'] = strip_tags(Str::markdown($post->body), '<p><ul><ol><li><strong><em><h3><br>');
         return view('single-post', ['post' => $post]);
     }
 
