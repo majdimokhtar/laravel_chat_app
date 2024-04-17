515077334c860187fbda712e1153a7db3e830383
Majdi Mokhtar <majdi.mokhtar@gmail.com>
2024-04-17 16:50:10 +0100

    user lists

--- app/Http/Controllers/FollowController.php
+++ app/Http/Controllers/FollowController.php
@@ -29,7 +29,8 @@ class FollowController extends Controller
         return back()->with('success', 'User successfully followed.');
     }
 
-    public function removeFollow() {
-
+    public function removeFollow(User $user) {
+        Follow::where([['user_id', '=', auth()->user()->id], ['followeduser', '=', $user->id]])->delete();
+        return back()->with('success', 'User succesfully unfollowed.');
     }
 }
--- resources/views/profile-posts.blade.php
+++ resources/views/profile-posts.blade.php
@@ -1,24 +1,5 @@
-<x-layout>
-  <div class="container py-md-5 container--narrow">
-      <h2>
-        <img class="avatar-small" src="{{$avatar}}" /> {{$username}}
-        <form class="ml-2 d-inline" action="/create-follow/{{$username}}" method="POST">
-          @csrf
-          <button class="btn btn-primary btn-sm">Follow <i class="fas fa-user-plus"></i></button>
-          <!-- <button class="btn btn-danger btn-sm">Stop Following <i class="fas fa-user-times"></i></button> -->
-          @if(auth()->user()->username == $username)
-          <a href="/manage-avatar" class="btn btn-secondary btn-sm">Manage Avatar</a>
-          @endif
-        </form>
-      </h2>
-
-      <div class="profile-nav nav nav-tabs pt-2 mb-4">
-        <a href="#" class="profile-nav-link nav-item nav-link active">Posts: {{$postCount}}</a>
-        <a href="#" class="profile-nav-link nav-item nav-link">Followers: 3</a>
-        <a href="#" class="profile-nav-link nav-item nav-link">Following: 2</a>
-      </div>
-
-      <div class="list-group">
+<x-profile :sharedData="$sharedData">
+  <div class="list-group">
         @foreach($posts as $post)
         <a href="/post/{{$post->id}}" class="list-group-item list-group-item-action">
           <img class="avatar-tiny" src="{{$post->user->avatar}}" />
@@ -26,5 +7,4 @@
         </a>
         @endforeach
       </div>
-    </div>
-</x-layout>
+</x-profile>
