a331301f32c79dbed10f28d4f01ca9b0f04ca0c0
Majdi Mokhtar <majdi.mokhtar@gmail.com>
2024-04-18 17:08:57 +0100

    search fct and bundling js

--- resources/views/profile-followers.blade.php
+++ resources/views/profile-followers.blade.php
@@ -1,4 +1,4 @@
-<x-profile :sharedData="$sharedData">
+<x-profile :sharedData="$sharedData" doctitle="{{$sharedData['username']}}'s Followers">
   <div class="list-group">
         @foreach($followers as $follow)
         <a href="/profile/{{$follow->userDoingTheFollowing->username}}" class="list-group-item list-group-item-action">
