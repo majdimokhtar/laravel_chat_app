ce0a8b7efec1c4296539f2e391baae4ff7bdbb70
Majdi Mokhtar <94905087+majdimokhtar@users.noreply.github.com>
2024-04-26 11:37:29 +0200

    Update layout.blade.php

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,7 +7,7 @@
     <title>@isset($doctitle)
       {{$doctitle}} | MyApp
       @else
-      OurApp
+      MyApp
       @endisset
     </title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
@@ -21,7 +21,7 @@
   <body>
     <header class="header-bar mb-3">
       <div class="container d-flex flex-column flex-md-row align-items-center p-3">
-        <h4 class="my-0 mr-md-auto font-weight-normal"><a href="/" class="text-white">OurApp</a></h4>
+        <h4 class="my-0 mr-md-auto font-weight-normal"><a href="/" class="text-white">MyApp</a></h4>
         
         @auth
         <div class="flex-row my-3 my-md-0">
